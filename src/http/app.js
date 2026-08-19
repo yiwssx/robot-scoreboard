f658d40929542c410d55ff6ae012f989c0d9c9fe
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-19 19:57:27 +0700

    wire field readiness into http app

--- src/http/app.js
+++ src/http/app.js
@@ -4,13 +4,13 @@ const express = require("express");
 const { responseHeaders } = require("./middleware/response-headers");
 const { createRouter } = require("./routes");
 
-function createApp({ scoreboard, publicDir }) {
+function createApp({ scoreboard, publicDir, fieldReadiness = null }) {
   const app = express();
 
   app.disable("x-powered-by");
   app.use(responseHeaders);
   app.use(express.json({ limit: "16kb" }));
-  app.use(createRouter({ scoreboard, publicDir }));
+  app.use(createRouter({ scoreboard, publicDir, fieldReadiness }));
   app.use(express.static(publicDir, { index: false }));
 
   return app;
