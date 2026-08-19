26bf08d1c503183efc34ff4add5b371abfc3f074
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-19 18:35:44 +0700

    serve organized page directory

--- src/http/controllers/pages.controller.js
+++ src/http/controllers/pages.controller.js
@@ -3,10 +3,10 @@
 const path = require("node:path");
 
 const PAGE_FILES = Object.freeze({
-  control: "control.html",
-  teamA: "team-a.html",
-  teamB: "team-b.html",
-  teams: "team-names.html",
+  control: path.join("pages", "control.html"),
+  teamA: path.join("pages", "team-a.html"),
+  teamB: path.join("pages", "team-b.html"),
+  teams: path.join("pages", "team-names.html"),
 });
 
 function createPagesController({ publicDir }) {
