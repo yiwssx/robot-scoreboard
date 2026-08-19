3d11fe29d1efbdb9ddcf4fe03a61ee94269ae791
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-19 19:57:37 +0700

    register field status routes

--- src/http/routes/index.js
+++ src/http/routes/index.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const { createHealthRouter } = require("./health.routes");
 const { createPagesRouter } = require("./pages.routes");
+const { createFieldStatusRouter } = require("./field-status.routes");
 
-function createRouter({ scoreboard, publicDir }) {
+function createRouter({ scoreboard, publicDir, fieldReadiness }) {
   const router = express.Router();
   router.use(createHealthRouter({ scoreboard }));
+  if (fieldReadiness) router.use(createFieldStatusRouter({ fieldReadiness, scoreboard }));
   router.use(createPagesRouter({ publicDir }));
   return router;
 }
