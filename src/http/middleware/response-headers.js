c2a0b6c7b54e45451d534ce21dd9d0337b594d70
Supharoek Sudadet <48784420+yiwssx@users.noreply.github.com>
2026-08-19 18:28:13 +0700

    extract response header middleware

---
+++ src/http/middleware/response-headers.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function responseHeaders(req, res, next) {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader("Referrer-Policy", "no-referrer");
+
+  if (/\.(?:html|css|js)$/i.test(req.path)) {
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+  }
+
+  next();
+}
+
+module.exports = { responseHeaders };
